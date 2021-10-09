1cdc3b1da7b2ebe7c130e30e8881857dd2982dca
Víctor Manuel Ortiz Guardeño <vikour92@gmail.com>
2021-10-09 16:48:28 +0200

    Cleanup step 3 added to import products job

--- ch01/src/test/java/es/vikour/sbia/ch01/ImportProductsIntegrationTest.java
+++ ch01/src/test/java/es/vikour/sbia/ch01/ImportProductsIntegrationTest.java
@@ -1,9 +1,8 @@
 package es.vikour.sbia.ch01;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-import java.math.BigDecimal;
-import java.util.List;
+import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,17 +15,25 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j2;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
 	"/import-products-job-context.xml",
 	"/test-context.xml"
 })
-@Log4j2
 public class ImportProductsIntegrationTest {
 	
+	private static final String INPUT_FILE = 
+			"classpath:/input/products.zip";
+	
+	private static final String INPUT_ERROR_FILE = 
+			"classpath:/input/productsWithError.zip";
+	
+	private static final String TARGET_DIRECTORY = 
+			"./target/importproductbatch/";
+	
+	private static final String TARGET_FILE = 
+			"products.txt";
+
 	@Autowired
 	private JobLauncher jobLauncher;
 	
@@ -46,12 +53,13 @@ public class ImportProductsIntegrationTest {
 		
 		jobLauncher.run(
 			job, new JobParametersBuilder()
-			 .addString("inputResource", "classpath:/input/products.zip")
-			 .addString("targetDirectory", "./target/importproductbatch/")
-			 .addString("targetFile", "products.txt")
+			 .addString("inputResource", INPUT_FILE)
+			 .addString("targetDirectory", TARGET_DIRECTORY)
+			 .addString("targetFile", TARGET_FILE)
 			 .toJobParameters());
 		
 		assertEquals(8,jdbcTemplate.queryForInt("select count(1) from product"));
+		assertTargetDirectoryDoesntExists();
 	}
 	
 	@Test
@@ -59,12 +67,19 @@ public class ImportProductsIntegrationTest {
 		
 		jobLauncher.run(
 			job, new JobParametersBuilder()
-			 .addString("inputResource", "classpath:/input/productsWithError.zip")
-			 .addString("targetDirectory", "./target/importproductbatch/")
-			 .addString("targetFile", "products.txt")
+			 .addString("inputResource", INPUT_ERROR_FILE)
+			 .addString("targetDirectory", TARGET_DIRECTORY)
+			 .addString("targetFile", TARGET_FILE)
 			 .toJobParameters());
 		
+		// Assertions
 		assertEquals(7,jdbcTemplate.queryForInt("select count(1) from product"));
+		assertTargetDirectoryDoesntExists();
+	}
+	
+	private void assertTargetDirectoryDoesntExists() {
+		File targetDirectory = new File(TARGET_DIRECTORY);
+		assertFalse(targetDirectory.exists());
 	}
 	
 
