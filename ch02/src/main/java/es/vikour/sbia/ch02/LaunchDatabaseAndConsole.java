99ae697c29abe7779c9a79615653620d862569b0
Víctor Manuel Ortiz Guardeño <vikour92@gmail.com>
2021-10-09 16:41:23 +0200

    Spring batch added successfully

--- ch02/src/main/java/es/vikour/sbia/ch02/LaunchDatabaseAndConsole.java
+++ ch02/src/main/java/es/vikour/sbia/ch02/LaunchDatabaseAndConsole.java
@@ -3,7 +3,6 @@ package es.vikour.sbia.ch02;
 import java.sql.SQLException;
 
 import org.h2.tools.Console;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class LaunchDatabaseAndConsole {
